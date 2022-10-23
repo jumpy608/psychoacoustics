f8a7bd1e8afccd823339d74ec951ce2700cd8582
mcswartz <64854941+mcswartz9@users.noreply.github.com>
2022-10-23 16:41:53 -0500

    Uploaded files for MainMenu button functionality

---
+++ Assets/Scripts/Main_Menu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class Main_Menu : MonoBehaviour
+{
+	public void LoadGame()
+	{
+		//Load the Game Scene
+		SceneManager.LoadScene(1); //0 = MainMenu, 1 = Game Scene
+	} 
+}
