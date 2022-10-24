dfdd293d42935e523b94fed657ada4132bcad16d
mcswartz <64854941+mcswartz9@users.noreply.github.com>
2022-10-23 20:12:15 -0500

    Commented MainMenu.cs

--- Assets/Scripts/Main_Menu.cs
+++ Assets/Scripts/Main_Menu.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class Main_Menu : MonoBehaviour
 {
+	// Name: LoadGame()
+    	// Programmer: Maggie Swartz
+    	// Date: 10/23/2022
+    	// Description: Utilizes SceneManagement library to switch between the scene environments (starts w/ main menu, then switches to game play). To be utilized w/ a button click.
 	public void LoadGame()
 	{
 		//Load the Game Scene
