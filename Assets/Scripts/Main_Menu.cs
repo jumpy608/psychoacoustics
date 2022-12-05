f698e48a85a1613cfc3afccdc11c234a0bc64c81
mcswartz <64854941+mcswartz9@users.noreply.github.com>
2022-12-04 20:52:21 -0600

    Added additional scene selection code

--- Assets/Scripts/Main_Menu.cs
+++ Assets/Scripts/Main_Menu.cs
@@ -5,13 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class Main_Menu : MonoBehaviour
 {
-	// Name: LoadGame()
+	// Name: LoadGame(), EaasySongs(), MediumSongs(), HardSongs(), BeginGame()
     	// Programmer: Maggie Swartz
     	// Date: 10/23/2022
-    	// Description: Utilizes SceneManagement library to switch between the scene environments (starts w/ main menu, then switches to game play). To be utilized w/ a button click.
-	public void LoadGame()
+    	// Description: Utilizes SceneManagement library to switch between the scene environments (starts w/ main menu, then switches based on selections). To be utilized w/ a button click.
+	public void MainMenu()
 	{
-		//Load the Game Scene
-		SceneManager.LoadScene(1); //0 = MainMenu, 1 = Game Scene
+		SceneManager.LoadScene(0); //0 = MainMenu
+	} 
+	
+	public void EasySongs()
+	{
+		SceneManager.LoadScene(1); //1 = Easy Song Select Scene
+	}
+
+	public void MediumSongs()
+	{
+		SceneManager.LoadScene(2); //2 = Medium Song Select Scene
+	}
+
+	public void HardSongs()
+	{
+		SceneManager.LoadScene(3); //3 = Hard Song Select Scene
+	}
+
+	public void BeginGame()
+	{	
+		SceneManager.LoadScene(4); //4 = Gameplay Scene
 	} 
 }
