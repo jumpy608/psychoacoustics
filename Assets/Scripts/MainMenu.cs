2b5f7b49a8435ac608dbbf015de0feb3afa1604b
Konrad Kahnert <kkahnert49@gmail.com>
2023-01-29 09:55:22 -0600

    added comments

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,7 +3,11 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-   public void Begin()
+    // Name: Begin Function
+    // Programmer: Konrad Kahnert
+    // Date: 1/29/2023
+    // Description: Switches to game scene
+    public void Begin()
     {
         SceneManager.LoadScene(1);
     }
