076bb4f55af19f95eaf58bcf55d8437cc7219583
mcswartz <64854941+mcswartz9@users.noreply.github.com>
2023-04-23 17:27:21 -0500

    Exit button script - fixed to work in editor

---
+++ Assets/Scripts/ButtonExit.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ButtonExit : MonoBehaviour
+{
+	// Name: Exit Function
+	// Programmer: Maggie Swartz
+	// Date: 4/23/23
+	// Description: Function allows exit button to quit the application and end the game
+	
+	public void Button_exit()
+	{
+		//Application.Quit(); This would exit when built in application
+		UnityEditor.EditorApplication.isPlaying = false; //This exits when still in editor
+		Debug.Log("Game is exiting!");
+	}
+}
