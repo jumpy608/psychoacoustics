5e91cb91fdbe8468062f77ef72a5b1bcf7fce966
mcswartz <64854941+mcswartz9@users.noreply.github.com>
2023-04-23 20:45:08 -0500

    Updated main menu script w/ return to main menu function

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Name: Begin Function
+    // Programmer: Konrad Kahnert
+    // Date: 1/29/2023
+    // Description: Switches to game scene
+    public void Begin()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Name: LevelGenerator Function
+    // Programmer: Maggie Swartz
+    // Date: 4/23/23
+    // Description: Allows level generator button to navigate to the correct scene
+    public void LevelGenerator()
+    {
+	SceneManager.LoadScene(2);
+    }
+
+    // Name: ReturnMain Function
+    // Programmer: Maggie Swartz
+    // Date: 4/23/23
+    // Description: Allows button clicked to return to main menu scene
+    public void ReturnMain()
+    {
+	SceneManager.LoadScene(0);
+    } 
+}
